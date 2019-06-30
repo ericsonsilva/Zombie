cb61ad28cc9e94d6c0050cf2b42a56c1bce89a35
ericsonsilva <ericson.silva@gmail.com>
2019-06-30 20:11:36 -0300

    Jogo terminado

--- main.lua
+++ main.lua
@@ -8,12 +8,20 @@ function love.load()
   sprites.background = love.graphics.newImage('sprites/background.png')
 -- tabela para movimento do plaer.
   player = {}
-  player.x = 200
-  player.y = 200
+  player.x = love.graphics.getWidth()/2
+  player.y = love.graphics.getHeight()/2
   player.speed = 180
 
   zombies = {}
   bullets = {}
+
+  gameState = 1
+  maxTime = 2
+  timer = maxTime
+
+  myFont = love.graphics.newFont(30)
+
+  score = 0
 end
 
 function love.update(dt)
@@ -21,20 +29,22 @@ function love.update(dt)
 -- DT define a contagem de segundos. Mutiplicando por DT
 -- garatimos que o jogo rodará sempre com a mesma velocidade,
 -- mesmo que os frames dropem.
-  if love.keyboard.isDown("s") then
-    player.y = player.y + player.speed * dt
-  end
+if gameState == 2 then
+  if love.keyboard.isDown("s") and player.y < love.graphics.getHeight() then
+      player.y = player.y + player.speed * dt
+    end
 
-  if love.keyboard.isDown("w") then
-    player.y = player.y - player.speed * dt
-  end
+    if love.keyboard.isDown("w") and player.y > 0 then
+      player.y = player.y - player.speed * dt
+    end
 
-  if love.keyboard.isDown("a") then
-    player.x = player.x - player.speed * dt
-  end
+    if love.keyboard.isDown("a") and player.x > 0 then
+      player.x = player.x - player.speed * dt
+    end
 
-  if love.keyboard.isDown("d") then
-    player.x = player.x + player.speed * dt
+    if love.keyboard.isDown("d") and player.x < love.graphics.getWidth() then
+      player.x = player.x + player.speed * dt
+    end
   end
 --faz os zumbis andarem até o jogador, calculando o angulo onde o jogador está.
 -- Usamos COS = cosseno e SIN = seno.
@@ -45,6 +55,9 @@ function love.update(dt)
     if distanceBetween(z.x, z.y, player.x, player.y) < 30 then
       for i,z in ipairs(zombies) do
         zombies[i] = nil
+        gameState = 1
+        player.x = love.graphics.getWidth()/2
+        player.y = love.graphics.getHeight()/2
       end
     end
   end
@@ -60,12 +73,13 @@ function love.update(dt)
       table.remove(bullets, i)
     end
   end
--- teste de colizão e remoção dos itens
+-- teste de colisão e remoção dos itens
   for i,z in ipairs(zombies) do
     for j,b in ipairs(bullets) do
       if distanceBetween(z.x, z.y, b.x, b.y) < 20 then
         z.dead = true
         b.dead = true
+        score = score + 1
       end
     end
   end
@@ -83,6 +97,15 @@ function love.update(dt)
       table.remove(bullets, i)
     end
   end
+
+  if gameState == 2 then
+    timer = timer - dt
+    if timer <= 0 then
+      spawnZombie()
+      maxTime = maxTime * 0.95
+      timer = maxTime
+    end
+  end
 end
 
 function love.draw()
@@ -90,6 +113,14 @@ function love.draw()
 -- nil retorna valor nulo ou default para o parâmetro.
 --getWidth e getHeight /2 mudam a referência do sprite pora o centro da imagem.
     love.graphics.draw(sprites.background, 0, 0)
+
+    if gameState == 1 then
+      love.graphics.setFont(myFont)
+      love.graphics.printf("Clique para começar!", 0, 50, love.graphics.getWidth(), "center")
+    end
+
+    love.graphics.printf("Score: " .. score, 0, love.graphics.getHeight() - 100, love.graphics.getWidth(), "center")
+
     love.graphics.draw(sprites.player, player.x, player.y, player_mouse_angle(), nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
 -- desenha zumbis na tela.
     for i,z in ipairs(zombies) do
@@ -115,7 +146,7 @@ function spawnZombie()
   zombie.y = 0
   zombie.speed = 100
   zombie.dead = false
--- zumbis spaunam fora da tela 
+-- zumbis spaunam fora da tela
   local side = math.random(1, 4)
 
   if side == 1 then
@@ -152,8 +183,15 @@ function love.keypressed(key, scancode, isrepeat)
 end
 --detecta que o botão esquedo do mouse foi apertado
 function love.mousepressed(x, y, button, isTouch)
-  if button == 1 then
+  if button == 1 and gameState ==2 then
     spawnBullets()
+  end
+
+  if gameState == 1 then
+    gameState = 2
+    maxTime = 2
+    timer = maxTime
+    score = 0
   end
 end
 
