847627835815f882b482eaac7dc1d1153d6e88de
ericsonsilva <ericson.silva@gmail.com>
2019-06-30 16:01:17 -0300

    desenhando player

--- main.lua
+++ main.lua
@@ -1,1 +1,13 @@
+function love.load()
+  sprites = {}
+  --para caminha de arquivos usar aspas simples.
+  sprites.player = love.graphics.newImage('sprites/player.png')
+end
 
+function love.update(dt)
+  -- body...
+end
+
+function love.draw()
+    love.graphics.draw(sprites.player, 100, 100)
+end
